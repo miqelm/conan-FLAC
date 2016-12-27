aa4364069fcff6eca84df07c251d69edfe05085c
Joseph D <joseph@outurnate.com>
2016-12-27 15:24:37 -0500

    please

--- conanfile.py
+++ conanfile.py
@@ -52,7 +52,7 @@ class FLACConan(ConanFile):
 
             arch = '--host=i686-pc-linux-gnu "CFLAGS=-m32" "CXXFLAGS=-m32" "LDFLAGS=-m32"' if self.settings.arch == "x86" else ""
             m32_pref = "setarch i386" if self.settings.arch == "x86" else ""
-            self.run('mkdir -p install && %s && chmod +x ./configure && %s %s ./configure --prefix=$(pwd)/../install %s' % (cd_build, env_line, m32_pref, arch))
+            self.run('mkdir -p install && %s && chmod +x ./configure && %s %s ./configure --prefix=$(pwd)/../install' % (cd_build, env_line, m32_pref))
             self.run("%s && %s make install" % (cd_build, env_line))
 
     def package(self):
